edb260eba0f091294febb45a1b549fb5fca9371a
Lorenzo Baldassarri <lb@digio.ch>
2021-11-24 17:13:30 +0100

    extend snapshot capabilities
    - add snapshot status
    - add GET endpoint

--- flow/snapshot.go
+++ flow/snapshot.go
@@ -2,23 +2,52 @@ package flow
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 )
 
 type SnapshotService interface {
+	Get(ctx context.Context, snapshotId Id) (*Snapshot, *Response, error)
 	List(ctx context.Context, options PaginationOptions) ([]*Snapshot, *Response, error)
 	Create(ctx context.Context, data *SnapshotCreate) (*Snapshot, *Response, error)
 	Delete(ctx context.Context, snapshotId Id) (*Response, error)
+}
+
+type SnaphostStatusKey = string
+
+const (
+	SnapshotStatusKeyAvailable SnaphostStatusKey = "available"
+	SnapshotStatusKeyCreating  SnaphostStatusKey = "creating"
+	SnapshotStatusKeyError     SnaphostStatusKey = "error"
+)
+
+type Status struct {
+	Id   Id                `json:"id"`
+	Name string            `json:"name"`
+	Key  string `json:"key"`
 }
 
 type Snapshot struct {
 	Id        Id       `json:"id"`
 	Name      string   `json:"name"`
 	Size      int      `json:"size"`
+	Status    Status   `json:"status"`
 	Volume    Volume   `json:"volume"`
 	Product   Product  `json:"product"`
 	CreatedAt DateTime `json:"created_at"`
+}
+
+func (snapshot *Snapshot) IsAvailable() (bool, error) {
+	switch snapshot.Status.Key {
+	case SnapshotStatusKeyAvailable:
+		return true, nil
+	case SnapshotStatusKeyCreating:
+		return false, nil
+	case SnapshotStatusKeyError:
+		return false, errors.New(fmt.Sprintf("Snapshot with id %d has errored state", snapshot.Id))
+	}
+	return false, errors.New(fmt.Sprintf("Snapshot with id %d has unknown status '%s'", snapshot.Id, snapshot.Status.Key))
 }
 
 type SnapshotCreate struct {
@@ -28,6 +57,24 @@ type SnapshotCreate struct {
 
 type snapshotService struct {
 	client *Client
+}
+
+func (s *snapshotService) Get(ctx context.Context, snapshotId Id) (*Snapshot, *Response, error) {
+	p := fmt.Sprintf("/v4/compute/snapshots/%d", snapshotId)
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, p, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var val *Snapshot
+
+	res, err := s.client.Do(req, &val)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return val, res, nil
 }
 
 func (s *snapshotService) List(ctx context.Context, options PaginationOptions) ([]*Snapshot, *Response, error) {
